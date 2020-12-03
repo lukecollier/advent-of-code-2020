36b02ca5ffa0c11832d273e29d0c73be3fea756c
Luke Collier <leccollier@gmail.com>
2020-12-03 23:55:58 +0000

    3rd part two complete

--- three/src/main.rs
+++ three/src/main.rs
@@ -3,9 +3,21 @@ const LINE_ENDING: &'static str = "\r\n";
 #[cfg(not(windows))]
 const LINE_ENDING: &'static str = "\n";
 
+fn count_tree_hits(map: &Vec<Vec<u8>>, right: &usize, down: &usize) -> u8 {
+    let (result, _) = map
+        .into_iter()
+        .step_by(*down)
+        .fold((0, 0), |(acc, pos), line| {
+            let wrapped_pos = pos % line.len();
+            let value = line.get(wrapped_pos).unwrap_or(&0);
+            (acc + value, pos + right)
+        });
+    result
+}
+
 fn main() {
     let input = include_str!("input.txt");
-    let (value, _) = input
+    let map = input
         .split(LINE_ENDING)
         .filter(|line| !line.is_empty())
         .map(|line: &str| {
@@ -18,12 +30,14 @@ fn main() {
                 .filter_map(Result::ok)
                 .collect::<Vec<u8>>()
         })
-        .fold((0, 0), |(acc, pos), line| {
-            let wrapped_pos = pos % line.len();
-            let value = line.get(wrapped_pos).unwrap_or(&0);
-            (acc + value, pos + 3)
-        });
-    println!("we would hit {}", value);
+        .collect::<Vec<Vec<u8>>>();
+    let part_one_trees_hit = count_tree_hits(&map, &3, &1);
+    let slopes: Vec<(usize, usize)> = vec![(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
+    let part_two_trees_hit = slopes.iter().fold(1, |acc, (right, down)| {
+        acc * usize::from(count_tree_hits(&map, right, down))
+    });
+    println!("part one we would hit {}", part_one_trees_hit);
+    println!("part two we would hit {}", part_two_trees_hit);
 }
 
 #[cfg(test)]
