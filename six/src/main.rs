329f1a32d4ffd0353ec4418b28dabbf1da2de2a1
Luke Collier <leccollier@gmail.com>
2020-12-06 21:05:30 +0000

    6th part one

---
+++ six/src/main.rs
@@ -0,0 +1,23 @@
+use std::collections::HashSet;
+
+#[cfg(windows)]
+const LINE_ENDING: &'static str = "\r\n";
+#[cfg(not(windows))]
+const LINE_ENDING: &'static str = "\n";
+
+fn main() {
+    let input: usize = include_str!("input.txt")
+        .split(&format!("{}{}", LINE_ENDING, LINE_ENDING))
+        .filter(|str| !str.is_empty())
+        .map(|str| {
+            str.chars()
+                .filter(|c| !c.is_whitespace())
+                .collect::<HashSet<char>>()
+                .into_iter()
+                .count()
+        })
+        .collect::<Vec<_>>()
+        .into_iter()
+        .sum();
+    println!("hello {:?}", input);
+}
