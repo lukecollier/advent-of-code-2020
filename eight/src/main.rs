89c740273b6c35877227ea2874c0deae557d1ba3
Luke Collier <leccollier@gmail.com>
2020-12-10 08:39:15 +0000

    improvements

--- eight/src/main.rs
+++ eight/src/main.rs
@@ -1,4 +1,5 @@
 use regex::Regex;
+use itertools::*;
 use std::env;
 use std::fs;
 
@@ -29,27 +30,82 @@ impl Operation {
         }
     }
 
-    fn run(program: Vec<Operation>) -> isize {
+    fn run_fix(program: &Vec<Operation>) -> isize {
+        let mut position: isize = 0;
+        let mut path: Vec<isize> = Vec::new();
+        while !path.contains(&(position + 1)) {
+            let operation = program.get(position as usize); 
+            match operation {
+                Some(Operation::Acc(_)) => {
+                    position = position + 1;
+                },
+                Some(Operation::Jmp(argument)) => {
+                    path.push(position);
+                    position = position + argument;
+                },
+                Some(Operation::Nop) => {
+                    path.push(position);
+                    position = position + 1;
+                },
+                None => panic!("error")
+            }
+        }
+        // if final position is a Nop change to Jmp and jump the amount of Jmp's ahead
+        let (change_position, change) = path.iter().sorted().rev().find_map(|pos| {
+            let length = program.len() as usize;
+            let position = pos.clone() as usize;
+            let operation = program.get(pos.clone() as usize);
+            let result: Option<(isize, Operation)> = match operation {
+                Some(Operation::Jmp(_)) => {
+                    let remaining_program = &program[(position + 1)..length].into_iter().filter(|op| match op {
+                        Operation::Jmp(_) => true,
+                        _ => false,
+                    }).count();
+                    if remaining_program == &0 {
+                        Some((position as isize, Operation::Nop))
+                    } else {
+                        None
+                    }
+                },
+                Some(Operation::Nop) => {
+                    let jump_to_final = program[(position + 1)..length].into_iter().enumerate().rev().find_map(|(idx, op)| match op {
+                        Operation::Jmp(_) => Some((position as isize, Operation::Jmp(idx as isize))),
+                        _ => None,
+                    });
+                    jump_to_final
+                },
+                _ => panic!("error"),
+            };
+            result
+        }).unwrap();
+        let mut fixed_program = program.clone();
+        std::mem::replace(&mut fixed_program[change_position as usize], change);
+        Operation::run(&fixed_program)
+    }
+
+    fn run(program: &Vec<Operation>) -> isize {
         let mut acc: isize = 0;
         let mut position: isize = 0;
-        let mut stack: Vec<isize> = Vec::new();
-        while !stack.contains(&(position + 1)) {
+        let mut path: Vec<isize> = Vec::new();
+        while !path.contains(&(position + 1)) {
             let operation = program.get(position as usize); 
             match operation {
                 Some(Operation::Acc(argument)) => {
                     position = position + 1;
                     acc = acc + argument;
-                    stack.push(position);
+                    path.push(position);
                 },
                 Some(Operation::Jmp(argument)) => {
                     position = position + argument;
-                    stack.push(position);
+                    path.push(position);
                 },
                 Some(Operation::Nop) => {
                     position = position + 1;
-                    stack.push(position);
+                    path.push(position);
                 },
-                None => panic!("error")
+                None => {
+                    path.push(position + 1);
+                }
             }
         }
         acc
@@ -67,8 +123,9 @@ fn main() {
                 .map(|str| Operation::parse_line(str))
                 .filter_map(Result::ok)
                 .collect::<Vec<Operation>>();
-            let output = Operation::run(program);
-            println!("output: {}", &output);
+            let output = Operation::run(&program);
+            let part_two_output = Operation::run_fix(&program);
+            println!("output: {} part_two_output: {}", &output, &part_two_output);
         }
         None => eprintln!("usage: needs input file"),
     };
